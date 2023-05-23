2e42080f6aad337451479a4a602984dbbb215960
Kathryn Verkhohliad <kathrine312003@gmail.com>
2023-05-23 20:21:16 +0300

    basic methods for AuthenticationService

---
+++ lib/src/services/helpers/service_controller.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'status_code_predicates.dart';
+
+import 'exceptions.dart';
+
+typedef ControllerLogic<T> = T Function(http.Response response);
+
+Future<T> httpServiceController<T>(
+    http.Response response, ControllerLogic<T> successLogic,
+    [ControllerLogic<T>? unsuccessfulLogic]) async {
+  //final res = await applyResponseMiddleware(response);
+
+  final res = response;
+
+  if (hasSuccessCode(res)) {
+    return successLogic(res);
+  }
+
+  if (unsuccessfulLogic != null) {
+    return unsuccessfulLogic(res);
+  }
+  throw ResourceNotFetchedException(res.reasonPhrase);
+}
