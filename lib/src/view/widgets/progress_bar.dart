5d4a7b220aedcf89bcaa9444dbc7b74848ae978f
Kathryn Verkhohliad <kathrine312003@gmail.com>
2023-05-24 16:56:17 +0300

    fixed progress bars on Dashboard

--- lib/src/view/widgets/progress_bar.dart
+++ lib/src/view/widgets/progress_bar.dart
@@ -1,47 +1,40 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-class ProgressBar extends StatefulWidget {
+class ProgressBar extends StatelessWidget {
   final int minValue;
   final int maxValue;
-  final int initialValue;
+  final int value;
   final double height;
   final Text? caption;
 
   const ProgressBar({
     required this.maxValue,
-    required this.initialValue,
+    required this.value,
     required this.height,
     this.minValue = 0,
     this.caption,
     super.key,
-  }) : // todo asserts, stateless
+  })  : assert(minValue < maxValue),
+        assert(value > minValue),
+        assert(value < maxValue),
         assert(minValue != maxValue);
 
   @override
-  State<ProgressBar> createState() => _ProgressBarState();
-}
-
-class _ProgressBarState extends State<ProgressBar> {
-  late int value = widget.initialValue;
-
-  @override
   Widget build(BuildContext context) {
-    final currentValue = value / (widget.maxValue - widget.minValue);
+    final currentValue = (value - minValue) / (maxValue - minValue);
 
     final progressBar = SizedBox(
-      height: widget.height,
+      height: height,
       child: LinearProgressIndicator(value: currentValue),
     );
 
-    if (widget.caption == null) return progressBar;
+    if (caption == null) return progressBar;
 
     return Stack(
       alignment: AlignmentDirectional.center,
       children: [
         progressBar,
-        widget.caption!,
+        caption!,
       ],
     );
   }
