58f45618fb4646b3ca0c5b8c84597880360aa8ae
Dmytro Popov <thedmitryp@ukr.net>
2023-05-27 00:11:16 +0100

    Created `Skill` model

---
+++ lib/src/data/models/skill.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+/// A class representing a user-defined skill with the information about its current [level].
+@JsonSerializable()
+class Skill {
+  /// A name of this skill, defined by the user during the skill creation.
+  final String name;
+
+  /// A level of this skill.
+  final int level;
+
+  /// An experience points which the current user has gained on this [level].
+  final int levelExp;
+
+  /// An id of this skill.
+  final int id;
+
+  const Skill({
+    required this.name,
+    required this.level,
+    required this.levelExp,
+    required this.id,
+  });
+}
