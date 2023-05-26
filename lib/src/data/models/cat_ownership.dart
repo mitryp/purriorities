296b3b88b45372bff6c8c207245e2825d63f7c64
Dmytro Popov <thedmitryp@ukr.net>
2023-05-27 00:11:06 +0100

    Created `CatOwnership` model

---
+++ lib/src/data/models/cat_ownership.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+/// A class representing the cat ownership of the current user.
+@JsonSerializable()
+class CatOwnership {
+  /// A level of the cat with the [catNameId] name-id owned by the current user.
+  final int level;
+
+  /// A date when the cat with the [catNameId] name-id was acquired bu the current user.
+  final DateTime acquireDate;
+
+  /// A name-id of this cat.
+  /// It is a string of the following look: `strawberry_cat` etc.
+  final String catNameId;
+
+  const CatOwnership({
+    required this.level,
+    required this.acquireDate,
+    required this.catNameId,
+  });
+}
